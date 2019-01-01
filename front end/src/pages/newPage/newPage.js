3bb3d713f1725f6a4655183f4e5360e773c17fc9
Coldstream-Louis <dushuyang@126.com>
2019-01-01 20:39:01 +0800

    前端初始化，Vue CLI多页面应用

---
+++ front end/src/pages/newPage/newPage.js
@@ -0,0 +1,8 @@
+import Vue from 'Vue'
+import newPage from './newPage.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(newPage)
+})
---
+++ front end/src/pages/newPage/newPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+       <newPage></newPage>
+    </div>
+</template>
+
+<script>
+import newPage from 'components/newPage.vue'
+export default {
+  name: 'app',
+  components: { newPage }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
